9826c46a4227545fb127d06e9a5df54b8d437ac1
Felix Tillyard <felixthecat90@hotmail.co.uk>
2017-07-16 20:35:22 +0100

    Open google calendar login and create an event.

--- google-calendar/tests/tests.py
+++ google-calendar/tests/tests.py
@@ -1,23 +1,40 @@
 import unittest
+import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class TestGoogleCalendar(unittest.TestCase):
+
     """
     This is a test class for testing google-calendar using selenium
     """
 
     def test_createEventButton(self):
         driver = webdriver.Chrome()
-        driver.get('http://www.python.org')
-        assert 'Python' in driver.title
-        elem = driver.find_element_by_name('q')
-        elem.clear()
-        elem.send_keys('pycon')
-        elem.send_keys(Keys.RETURN)
-        assert 'No results found.' not in driver.page_source
+        driver.get('http://calendar.google.com')
+        elem_username = driver.find_element_by_name('identifier')
+        elem_username.clear()
+        elem_username.send_keys(os.environ['GOOGLE_USERNAME'])
+        driver.find_element_by_id('identifierNext').click()
+        time.sleep(5)
+        elem_password = driver.find_element_by_name('password')
+        elem_password.clear()
+        elem_password.send_keys(os.environ['GOOGLE_PASSWORD'])
+        driver.find_element_by_id('passwordNext').click()
+        time.sleep(5)
+        driver.find_element_by_css_selector('#createEventButtonContainer > div > div.goog-inline-block.goog-imageless-button.goog-imageless-button-collapse-left.qnb-qab').click()
+        elem_qa_event_name = driver.find_element_by_id(':x.textarea')
+        elem_qa_event_name.clear()
+        elem_qa_event_name.send_keys('Dinner at the Ritz')
+        driver.find_element_by_css_selector('body > div.qab-container.gcal-popup > div').click()
+        time.sleep(5)
+        driver.find_element_by_css_selector(r'#\3a 1p\2e save_top > div').click()
+        time.sleep(5)
         driver.close()
 
 if __name__ == '__main__':
